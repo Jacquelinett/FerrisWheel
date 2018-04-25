5a38e54bc1534fddc892cd7b92a0da1e34f1e92d
Jacqueline <jacqueline@ordrstudio.com>
2018-04-25 11:31:16 -0400

    Finished app

--- SimpleDemonstration/GameScene.swift
+++ SimpleDemonstration/GameScene.swift
@@ -11,63 +11,167 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var background : SKSpriteNode!
+    var ferrisWheel : SKSpriteNode!
+    var ferrisHandle : SKSpriteNode!
+    
+    var spinCount = 0
+    var tempCount = 0;
+    var tempValue = 0;
+    
+    weak var viewController: GameViewController!
     
     override func didMove(to view: SKView) {
+        background = SKSpriteNode(imageNamed: "background.jpeg")
+        background.position = CGPoint(x: 0, y: 0)
+        background.physicsBody?.pinned = true
+        self.addChild(background)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        ferrisWheel = SKSpriteNode(imageNamed: "ferris.jpeg")
+        ferrisWheel.position = CGPoint(x: frame.midX, y: frame.midY + 10)
+        ferrisWheel.physicsBody?.pinned = true
+        self.addChild(ferrisWheel)
+        
+        ferrisHandle = SKSpriteNode(imageNamed: "handle.jpeg")
+        ferrisHandle.position = CGPoint(x: frame.midX, y: frame.midY + 10)
+        ferrisHandle.physicsBody?.pinned = true
+        self.addChild(ferrisHandle)
+    }
+    
+    func spinRectangleXTime(time : Int) {
+        spinCount = time;
+        viewController.lblStatus.textColor = UIColor.yellow
+        viewController.lblStatus.text = "Loop in progress...";
+        viewController.lblSliderValue.text = "Slider Value = \(time)";
+        viewController.lblCurrentLoop.text = "Current Loop = 0";
+        viewController.lblEvaluate.text = "";
+        tempCount = 0;
+        tempValue = time;
+        repeatRunning()
+    }
+    
+    func repeatRunning() {
+        
+        let wait500 = SKAction.wait(forDuration: 0.5)
+        
+        viewController.lblCurrentLoop.text = "Current Loop = \(tempCount)";
+        
+        if (spinCount > 0) {
+            spinCount -= 1;
+            viewController.lblCondition.textColor = UIColor.yellow
+            viewController.lblEvaluate.textColor = UIColor.yellow
+            viewController.lblEvaluate.text = "\(tempCount) < \(tempValue)";
+            self.run(wait500, completion: evaluate1)
         }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        else {
+            viewController.lblSpinAction.textColor = UIColor.white
+            viewController.lblCondition.textColor = UIColor.yellow
+            viewController.lblEvaluate.textColor = UIColor.yellow
+            viewController.lblEvaluate.text = "\(tempCount) < \(tempValue)";
+            self.run(wait500, completion: fEvaluate1)            
         }
     }
     
+    func fEvaluate1() {
+        let wait500 = SKAction.wait(forDuration: 0.5)
+        viewController.lblEvaluate.text = "\(tempCount) < \(tempValue).";
+        self.run(wait500, completion: fEvaluate2)
+    }
+    
+    func fEvaluate2() {
+        let wait500 = SKAction.wait(forDuration: 0.5)
+        viewController.lblEvaluate.text = "\(tempCount) < \(tempValue)..";
+        self.run(wait500, completion: fEvaluate3)
+    }
+    
+    func fEvaluate3() {
+        let wait500 = SKAction.wait(forDuration: 0.5)
+        viewController.lblEvaluate.text = "\(tempCount) < \(tempValue)...";
+        self.run(wait500, completion: fEvaluateFinal)
+    }
+    
+    func fEvaluateFinal() {
+        viewController.lblEvaluate.textColor = UIColor.red
+        viewController.lblEvaluate.text = "\(tempCount) < \(tempValue) FALSE";
+        
+        viewController.lblStatus.textColor = UIColor.green
+        viewController.lblStatus.text = "DONE";
+    }
+    
+    func evaluate1() {
+        let wait500 = SKAction.wait(forDuration: 0.5)
+        viewController.lblEvaluate.text = "\(tempCount) < \(tempValue).";
+        self.run(wait500, completion: evaluate2)
+    }
+    
+    func evaluate2() {
+        let wait500 = SKAction.wait(forDuration: 0.5)
+        viewController.lblEvaluate.text = "\(tempCount) < \(tempValue)..";
+        self.run(wait500, completion: evaluate3)
+    }
+    
+    func evaluate3() {
+        let wait500 = SKAction.wait(forDuration: 0.5)
+        viewController.lblEvaluate.text = "\(tempCount) < \(tempValue)...";
+        self.run(wait500, completion: evaluateFinal)
+    }
+    
+    func evaluateFinal() {
+        let wait500 = SKAction.wait(forDuration: 0.5)
+        
+        viewController.lblEvaluate.textColor = UIColor.green
+        viewController.lblEvaluate.text = "\(tempCount) < \(tempValue) TRUE";
+        
+        self.run(wait500, completion: beginSpin)
+    }
+    
+    func beginSpin() {
+        let spin = SKAction.rotate(byAngle: CGFloat.pi * 2, duration: 1)
+        
+        viewController.lblCondition.textColor = UIColor.white
+        viewController.lblEvaluate.textColor = UIColor.white
+        viewController.lblSpinAction.textColor = UIColor.yellow
+        
+        ferrisWheel.run(spin, completion: stopSpin)
+    }
+    
+    func stopSpin() {
+        let wait500 = SKAction.wait(forDuration: 0.5)
+        
+        viewController.lblSpinAction.textColor = UIColor.white
+        viewController.lblActionAdd1.textColor = UIColor.yellow
+        
+        self.run(wait500, completion: incrementTemp)
+    }
+    
+    func incrementTemp() {
+        let wait500 = SKAction.wait(forDuration: 0.5)
+        
+        tempCount = tempCount + 1;
+        viewController.lblEvaluate.text = "\(tempCount) < \(tempValue)";
+        
+        self.run(wait500, completion: briefWait)
+    }
+    
+    func briefWait() {
+        viewController.lblActionAdd1.textColor = UIColor.white
+        repeatRunning()
+    }
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
--- SimpleDemonstration/GameViewController.swift
+++ SimpleDemonstration/GameViewController.swift
@@ -11,27 +11,56 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
+    @IBOutlet weak var rotationSlider: UISlider!
+    @IBOutlet weak var rotationButton: UIButton!
+    @IBOutlet weak var rotationLabel: UILabel!
+    
+    @IBOutlet weak var lblCondition: UILabel!
+    @IBOutlet weak var lblSpinAction: UILabel!
+    @IBOutlet weak var lblActionAdd1: UILabel!
+    @IBOutlet weak var lblEvaluate: UILabel!
+    @IBOutlet weak var lblCurrentLoop: UILabel!
+    @IBOutlet weak var lblSliderValue: UILabel!
+    @IBOutlet weak var lblStatus: UILabel!
+    
+    
+    var currentGame: GameScene!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        currentGame = GameScene(fileNamed: "GameScene")
+        currentGame.viewController = self
+        
+        currentGame.scaleMode = .aspectFill
+        
+        rotationButton.layer.borderColor = UIColor.cyan.cgColor
+        rotationButton.layer.borderWidth = 2
+        rotationButton.layer.backgroundColor = UIColor.darkGray.cgColor
+
+        print(currentGame);
+        
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            view.presentScene(currentGame)
             
-            view.ignoresSiblingOrder = true
+            view.ignoresSiblingOrder = false
             
-            view.showsFPS = true
-            view.showsNodeCount = true
+            view.showsFPS = false
+            view.showsNodeCount = false
+            
         }
     }
-
+    
+    @IBAction func onSlideChanged(_ sender: Any) {
+        rotationLabel.text = "Number of Rotation: " + Int(rotationSlider.value).description;
+        rotationSlider.value = floor(rotationSlider.value)
+    }
+    
+    @IBAction func buttonUpInside(_ sender: Any) {
+        currentGame.spinRectangleXTime(time: Int(rotationSlider.value))
+    }
+    
     override var shouldAutorotate: Bool {
         return true
     }
